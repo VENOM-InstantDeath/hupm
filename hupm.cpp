65465c782e9665b603df16fdc1c6f90be6c803a2
VENOM-InstantDeath <mohathepro111@gmail.com>
2021-08-20 00:41:46 -0300

    Updated hupm

--- hupm.cpp
+++ hupm.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string.h>
+#include <algorithm>
+#include <vector>
 #include <curl/curl.h>
 using namespace std;
 
@@ -29,11 +31,39 @@ int main(int argc, char *argv[]) {
 	}
 	if (!strcmp(argv[1], "i") || !strcmp(argv[1], "install")) {
 		cout << "Install\n";
+		cout << argc << endl;
 		//argv[2]
+		vector<string> insopts = {"--local", "-l"};
+		int setting[] = {0}; //0-local
 		if (argc < 3) {
 			cout << "hupm: \033[1;31mno targets specified\033[0m\n";
 			cout << "Try \033[1;32m`hupm --help`\033[0m for more information\n";
 			return 1;
+		}
+		for (int i=2; i<argc; i++) {
+			if (argv[i][0] == '-') {
+				cout << "Opcion: " << argv[i] << endl;
+				if (find(insopts.begin(), insopts.end(), argv[i]) == insopts.end()) {
+					cout << "hupm: \033[1;31minvalid option: " << argv[i] << "\033[0m\n";
+					return 1;
+				}
+			}
+			if (argv[i] == "--local" || argv[i] == "-l") {
+				cout << "Local\n";
+				setting[0] = 1;
+			}
+		}
+		for (int i=2; i<argc; i++) {
+			if (! (argv[i][0] == '-')) {
+				cout << argv[i] << endl;
+				//directorio -- /var/lib/hupm/ packs-data
+				if (!setting[0]) {
+					curl = curl_easy_init();
+					if (curl) {
+						curl_easy_setopt(curl, CURLOPT_URL, "https://raw.githubusercontent.com/VENOM-InstantDeath/");
+					}
+				}
+			}
 		}
 	} else if (!strcmp(argv[1], "r") || !strcmp(argv[1], "remove")) {
 		cout << "Remove\n";
@@ -46,7 +76,7 @@ int main(int argc, char *argv[]) {
 	} else if (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")) {
 		cout << "Help\n";
 	} else {
-		cout << "hupm: \033[1;31minvalid option '" << argv[1] << "'\033[0m" << endl;
+		cout << "hupm: \033[1;31minvalid operation: '" << argv[1] << "'\033[0m" << endl;
 		return 1;
 	}
 
