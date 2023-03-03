e185099ea48aee96f9593b00285f359f35301d5e
VENOM-InstantDeath <mohathepro111@gmail.com>
2023-03-03 15:39:56 -0300

    Progress 3/3

--- src_hupm.cpp
+++ src_hupm.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <sstream>
+#include <fstream>
 #include <string.h>
+#include <dirent.h>
+#include <sqlite3.h>
+#include <unistd.h>
+#include <json.h>
 #include <curl/curl.h>
 using namespace std;
 
@@ -8,6 +14,8 @@ size_t strappnet(char* ptr, size_t x, size_t y, string *str) {
 	str->append(ptr);
 	return 0;
 }
+
+int checkdb();
 
 int main(int argc, char **argv) {
 	const char* operation = nullptr; /*install update refresh remove*/
@@ -45,14 +53,51 @@ int main(int argc, char **argv) {
 			if (operation==nullptr) {operation="remove";} else {cout<<"\033[1;31mError\033[0m: More than one command specified.\n";}}
 		else {cout << "\033[1;31mError\033[0m: argument '" << argv[i] << "' is invalid."<<endl;return 1;}
 	}
+	/*Check for invalid set of options*/
+	if (options[0] && options[1]) {
+		puts("\033[1;31mError\033[0m: Invalid set of options");
+	}
+	/*start dealing with the operations*/
 	if (operation == nullptr) {
 		puts("\033[1;31mError\033[0m: No command specified");
 		return 1;
 	}
-	/*Check for invalid set of options*/
-
+	if (operation == "refresh") {
+		/* Check permissions */
+		if (getuid()) {std::cout << "\033[1;31mError\033[0m: Insufficient permissions." << std::endl;return 1;}
+		/*Read config*/
+		std::ifstream conf("/var/lib/hupm/conf.json");
+		std::stringstream jstr;
+		jstr << conf.rdbuf();
+		/* In a loop, we download each db*/
+		json_object *jobj = json_tokener_parse(jstr.str().c_str());
+		json_object_object_foreach(jobj, key, val) {
+			json_object *kobj = json_object_object_get(jobj, key);
+			std::string dbname = key; dbname += ".db";
+			std::string url = json_object_get_string(kobj);
+			url += dbname;
+			/*download it now*/
+			std::cout << "Downloading " << key <<"... ";
+			FILE *fdb = fopen(dbname.c_str(), "w+");
+			CURL *curl = curl_easy_init();
+			CURLcode res;
+			curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+			curl_easy_setopt(curl, CURLOPT_WRITEDATA, fdb);
+			res = curl_easy_perform(curl);
+			if (res == CURLE_OK) {
+				std::cout << "\033[1;32mOK\033[0m\n";
+			} else {
+				std::cout << "\033[1;31mError\033[0m\n";
+			}
+			curl_easy_cleanup(curl);
+		}
+		/* file and in the same cycle we write it. (overwrite)*/
+	}
+	if (operation == "install");
+	if (operation == "update");
+	if (operation == "remove");
 	/*CURL *curl = curl_easy_init();
-	curl<_easy_setopt(curl, CURLOPT_URL);
+	curl_easy_setopt(curl, CURLOPT_URL);
 	curl_easy_setopt(curl, CURLOPT_WRITEDATA);
 	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION);*/
 	return 0;
